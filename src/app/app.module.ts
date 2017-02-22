4b9b2e599626ef6fddcc77ee3fb87260adef1841
Irin <averyirin@gmail.com>
2017-02-22 09:25:44 -0500

    Created firebase connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
 
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyCnML7O2OVlOmhGCMBmLeRYGUwLcWFg7t8",
+    authDomain: "comp3025-shoppinglist.firebaseapp.com",
+    databaseURL: "https://comp3025-shoppinglist.firebaseio.com",
+    storageBucket: "comp3025-shoppinglist.appspot.com",
+    messagingSenderId: "989456491585"
+};
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
